63eb4278999c8857e7a7bd23005729e0e9d4c69d
Isciaku <136129514+Isciaku@users.noreply.github.com>
2025-09-02 11:21:29 -0400

    Create Home.jsx

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+<main>
+    <section className="homehero">
+        <div className="homeheroinfo"></div>
+        <div className="homeherocta"></div>        
+    </section>
+    <section className="homethemesections">
+        <div className="homeAbout"></div>
+        <div className="homegetinvolved"></div>
+        <div className="homelisteningsession"></div>
+        <div className="homementalhealth"></div>
+        <div className="homedonate"></div>
+    </section>
+</main>
